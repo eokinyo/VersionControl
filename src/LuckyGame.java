04d7ef63a9750ac0bb7acec949ffe881c726adda
eokinyo <50834185+eokinyo@users.noreply.github.com>
2023-10-03 07:05:19 +0300

    Step 1: Generate and display three random numbers between 1 and 10

--- src/LuckyGame.java
+++ src/LuckyGame.java
@@ -3,8 +3,17 @@ import java.util.Random; //To import the random number generator utility
 public class LuckyGame {
 
 	public static void main(String[] args) {
-
-
+		
+		int playerMoney = 5; //The initial amount of money in € that the player has.
+        Random random = new Random();
+        
+        while (playerMoney > 0) {
+            // Step 1: Generate and display three random numbers between 1 and 10
+            int num1 = random.nextInt(10) + 1;
+            int num2 = random.nextInt(10) + 1;
+            int num3 = random.nextInt(10) + 1;
+            
+            System.out.println("Random numbers: " + num1 + ", " + num2 + " and " + num3);
 
 	}
 
